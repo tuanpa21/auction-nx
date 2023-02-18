10434e2ced336867dfa91d9e15194be4145f58ad
Pham Anh Tuan <phamtuan210394@gmail.com>
2023-02-18 16:40:55 +0100

    fix: handle popup

--- libs/client/components/dialog/src/lib/menu/interface.d.ts
+++ libs/client/components/dialog/src/lib/menu/interface.d.ts
@@ -1,4 +1,9 @@
 import { ForwardRefExoticComponent, SVGProps } from 'react';
+import {
+  ArrowLeftIcon,
+  ArrowLeftOnRectangleIcon,
+  PlusCircleIcon,
+} from '@heroicons/react/24/outline';
 
 export interface MenuItem {
   id: string;
@@ -11,3 +16,24 @@ export interface MenuItem {
 interface MenuProps {
   items: MenuItem[];
 }
+
+export const profile: MenuItem[] = [
+  {
+    id: 'create',
+    name: 'Create',
+    icon: PlusCircleIcon,
+    href: '/create',
+  },
+  {
+    id: 'deposit',
+    name: 'Deposit',
+    icon: ArrowLeftIcon,
+    href: '/deposit',
+  },
+  {
+    id: 'logout',
+    name: 'Logout',
+    icon: ArrowLeftOnRectangleIcon,
+    href: '/logout',
+  },
+];
