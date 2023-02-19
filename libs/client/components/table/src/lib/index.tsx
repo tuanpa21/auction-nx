74dc4a0a1ea77e38e9d9e67ea9760b4761eccf09
tuanpa21 <phamtuan210394@gmail.com>
2023-02-19 18:30:23 +0700

    feat: dashboard implementation (#7)

---
+++ libs/client/components/table/src/lib/index.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface ClientComponentsTableProps {}
+
+export function ClientComponentsTable(props: ClientComponentsTableProps) {
+  return (
+    <div>
+      <h1>Welcome to ClientComponentsTable!</h1>
+    </div>
+  );
+}
+
+export default ClientComponentsTable;
