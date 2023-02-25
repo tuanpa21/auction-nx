20d6a268f25cc4c170437ac53b1b689f43210b67
Pham Anh Tuan <phamtuan210394@gmail.com>
2023-02-25 21:17:19 +0700

    Prevent random router access login

--- libs/client/pages/auth/src/lib/index.tsx
+++ libs/client/pages/auth/src/lib/index.tsx
@@ -1,10 +1,18 @@
 import { useParams } from 'react-router-dom';
 
-import { useAuthData } from './data/index';
+import { useAuthData } from './data';
 import AuthView from './view/index';
 
 export default function Auth() {
   const { type } = useParams();
+  if (type !== 'login' && type !== 'register') {
+    return (
+      <div id="error-page">
+        <h1>Oops!</h1>
+        <p>The page you're trying to access isn't exist.</p>
+      </div>
+    );
+  }
 
   const { touched, values, errors, handleChange, handleSubmit, isLoading } =
     useAuthData(type);
