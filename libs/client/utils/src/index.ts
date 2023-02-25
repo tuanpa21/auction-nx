a6099e81eb494275c2222676764626c23180fd03
Pham Anh Tuan <phamtuan210394@gmail.com>
2023-02-25 18:38:12 +0700

    Updating utils lib functions

--- libs/client/utils/src/index.ts
+++ libs/client/utils/src/index.ts
@@ -1,4 +1,5 @@
-export * from './lib';
+export * from './lib/httpClient';
 export * from './lib/env';
 export { default as queryClient } from './lib/queryClient';
 export { default as changeState } from './lib/route';
+export * from './lib/auth';
---
+++ libs/client/utils/src/lib/auth.ts
@@ -0,0 +1,45 @@
+// set token to local storage
+export function setToken(token: string) {
+  localStorage.setItem('token', token);
+}
+
+// get token from local storage
+export function getToken() {
+  return localStorage.getItem('token');
+}
+
+// set refresh token to local storage
+export function setRefreshToken(refreshToken: string) {
+  localStorage.setItem('refreshToken', refreshToken);
+}
+
+// get refresh token from local storage
+export function getRefreshToken() {
+  return localStorage.getItem('refreshToken');
+}
+
+// set expires in to local storage
+export function setExpiresIn(expiresIn: string) {
+  localStorage.setItem('expiresIn', expiresIn);
+}
+
+// get expires in from local storage
+export function getExpiresIn() {
+  return localStorage.getItem('expiresIn');
+}
+
+export function setUser(user: any) {
+  localStorage.setItem('user', JSON.stringify(user));
+}
+
+export function getUser() {
+  const user = localStorage.getItem('user');
+  if (user) return JSON.parse(user);
+  return null;
+}
+
+export function removeToken() {
+  localStorage.removeItem('token');
+  localStorage.removeItem('refreshToken');
+  localStorage.removeItem('expiresIn');
+}
