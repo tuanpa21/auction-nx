4a3cd24e0dedf6aa7b9b93268cd951f5e041f66d
Pham Anh Tuan <phamtuan210394@gmail.com>
2023-02-26 16:22:39 +0100

    fix: move date utilities

--- libs/client/utils/src/lib/httpClient.ts
+++ libs/client/utils/src/lib/httpClient.ts
@@ -58,24 +58,3 @@ export async function httpClient<T, K>(requestConfig: AxiosRequestConfig<T>) {
   }
 }
 
-export function addSeconds(date: Date, seconds: number) {
-  const dateCopy = new Date(date.getTime());
-
-  dateCopy.setSeconds(dateCopy.getSeconds() + seconds);
-
-  return dateCopy;
-}
-
-export function subtractHours(date: Date, hours: number) {
-  date.setHours(date.getHours() - hours);
-
-  return date;
-}
-
-export const getTimeBetweenDates = (startDate: Date, endDate: Date) => {
-  const seconds = Math.floor((endDate.getTime() - startDate.getTime()) / 1000);
-  const minutes = Math.floor(seconds / 60);
-  const hours = Math.floor(minutes / 60);
-  const days = Math.floor(hours / 24);
-  return { seconds, minutes, hours, days };
-};
