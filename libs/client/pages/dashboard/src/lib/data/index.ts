ec63d0e3f1ed2d5b0f93ed0d886f9b770ac7cdb1
Pham Anh Tuan <phamtuan210394@gmail.com>
2023-02-27 11:18:19 +0100

    fix: move project to data folder

--- libs/client/pages/dashboard/src/lib/data/index.ts
+++ libs/client/pages/dashboard/src/lib/data/index.ts
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { Bid } from './utils';
+import { getTimeBetweenDates } from '@auction-nx/client/utils';
 
 export function useDashboardData() {
   const [open, setOpen] = useState({
@@ -9,9 +11,28 @@ export function useDashboardData() {
 
   const tabs = ['On Going', 'Completed'];
 
+  const bidCheck = (bid: Bid) => {
+    if (bid.auctions?.length) {
+      const currentItemBidDate = new Date(
+        bid.updatedAt || new Date().toUTCString()
+      );
+      const incomingItemBidDate = new Date(new Date().toUTCString());
+      const diff = getTimeBetweenDates(currentItemBidDate, incomingItemBidDate);
+
+      return diff.seconds < 5 || bid.status === 'COMPLETE';
+    } else {
+      if (bid.status === 'COMPLETE') {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  };
+
   return {
     tabs,
     setOpen,
     open,
+    bidCheck,
   };
 }
