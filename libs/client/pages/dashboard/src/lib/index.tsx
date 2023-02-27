618aa7b65597f99f8e31bf76de692cfe12894dcf
tuanpa21 <phamtuan210394@gmail.com>
2023-02-27 17:58:55 +0700

    fix: check auction data before throwing an error in bid button (#19)
    * fix: refetch item data
    
    * fix: check auction data before throwing an error in bid button
    
    * remove env
    
    * fix: move project to data folder
    
    * adjust env

--- libs/client/pages/dashboard/src/lib/index.tsx
+++ libs/client/pages/dashboard/src/lib/index.tsx
@@ -2,7 +2,14 @@ import DashboardView from './view';
 import { useDashboardData } from './data';
 
 export default function Dashboard() {
-  const { tabs, open, setOpen } = useDashboardData();
+  const { tabs, open, setOpen, bidCheck } = useDashboardData();
 
-  return <DashboardView tabs={tabs} open={open} setOpen={setOpen} />;
+  return (
+    <DashboardView
+      tabs={tabs}
+      open={open}
+      setOpen={setOpen}
+      bidCheck={bidCheck}
+    />
+  );
 }
